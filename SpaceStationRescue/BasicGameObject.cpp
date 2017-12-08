2d4be172de263db4265c954100e0d427214accf4
Kevin Boylan <idakev2396@gmail.com>
2017-12-08 10:39:24 +0000

    Added in Basic Game Object

--- SpaceStationRescue/BasicGameObject.cpp
+++ SpaceStationRescue/BasicGameObject.cpp
@@ -1,1 +1,29 @@
 #include "BasicGameObject.h"
+
+BasicGameObject::BasicGameObject()
+{
+
+}
+
+BasicGameObject::BasicGameObject(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f maxSpeed, float orientation, sf::Texture tex)
+{
+	m_position = pos;
+	m_velocity = vel;
+	m_maxSpeed = maxSpeed;
+	m_orientation = orientation;
+	m_texture = tex;
+}
+
+BasicGameObject::~BasicGameObject()
+{
+}
+
+void BasicGameObject::Update()
+{
+	m_position += m_velocity;
+}
+
+void BasicGameObject::Draw(sf::RenderWindow *window)
+{
+	window->draw(m_sprite);
+}
--- SpaceStationRescue/BasicGameObject.h
+++ SpaceStationRescue/BasicGameObject.h
@@ -1,3 +1,25 @@
 #pragma once
 
 // Position, velocity. texture, sprite, orientation/rotation, max speed, draw, update
+
+#include "SFML\Graphics.hpp"
+
+
+class BasicGameObject
+{
+protected:
+	sf::Vector2f m_position;
+	sf::Vector2f m_velocity;
+	sf::Vector2f m_maxSpeed;
+
+	float m_orientation;
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+
+public:
+	BasicGameObject();
+	BasicGameObject(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f maxSpeed, float orientation, sf::Texture tex);
+	~BasicGameObject();
+	void Update();
+	void Draw(sf::RenderWindow *window);
+};
